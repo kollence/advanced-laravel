e32bb7c369cdc696e355af14990cf6249605e956
kole <misterija@gmail.com>
2023-08-14 17:55:22 +0200

    test if assertInstanceOf thread

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class ThreadTest extends TestCase
-{
+{   // fresh migrations automaticaly migrate:fresh
     use DatabaseMigrations;
     /**
      * A basic unit test example.
@@ -16,36 +16,33 @@ class ThreadTest extends TestCase
      * @return void
      */
     protected $thread;
+
     public function setUp() : void
     {
         parent::setUp();    
-
+        //initialize one $thread as ready & created with factory
         $this->thread = \App\Models\Thread::factory()->create();
     }
 
     public function test_thread_has_reply()
     {
-        $thread = \App\Models\Thread::factory()->create();
-
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
+        // replies are assertInstanceOf thread
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
     }
 
     public function test_thread_has_creator()
     {
-        $thread = \App\Models\Thread::factory()->create();
-
-        $this->assertInstanceOf('App\Models\User', $thread->user);
+        // user are assertInstanceOf thread
+        $this->assertInstanceOf('App\Models\User', $this->thread->user);
     }
 
     public function test_thread_can_add_reply()
     {
-        $thread = \App\Models\Thread::factory()->create();
-
-        $thread->addReply([
+        $this->thread->addReply([
             'body' => 'Foobar',
             'user_id' => 1
         ]);
 
-        $this->assertCount(1, $thread->replies);
+        $this->assertCount(1, $this->thread->replies);
     }
 }

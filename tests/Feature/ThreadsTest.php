0844052254b00059ba1db5fe5e742991c1e33277
kole <misterija@gmail.com>
2023-08-14 17:23:18 +0200

    guest_can_read_replies_that_are_associated_with_a_thread

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -27,21 +27,23 @@ class ThreadsTest extends TestCase
 
     public function test_guest_can_browse_threads()
     {
-        // go to route & see if created thred can be seen on page /threads
+        // go to route & see if created thread can be seen on page /threads
         $this->get('/threads')
             ->assertSee($this->thread->title);
     }
 
-    public function test_user_can_read_a_single_thread()
+    public function test_guest_can_read_a_single_thread()
     {
+        // go to route & see if created single thread can be seen on page /threads/{id}
         $this->get('/threads/' . $this->thread->id)
             ->assertSee($this->thread->title);
     }
 
-    public function test_user_can_read_replies_that_are_associated_with_a_thread()
+    public function test_guest_can_read_replies_that_are_associated_with_a_thread()
     {
+        //create reply
         $reply = \App\Models\Reply::factory()->create(['thread_id' => $this->thread->id]);
-
+        // go to route & see if created reply can be seen on page /threads/{id}
         $response = $this->get('/threads/' . $this->thread->id);
         $response->assertSee($reply->body);
     }

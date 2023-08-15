d8406912a97bced759b9ab88eed9e18b14eb782c
kole <misterija@gmail.com>
2023-08-15 16:47:29 +0200

    fillable & relationship & method path

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -10,7 +10,7 @@ class Thread extends Model
     use HasFactory;
 
     // protected $guarded = ['id'];
-    protected $fillable = ['user_id', 'title', 'body'];
+    protected $fillable = ['user_id', 'channel_id', 'title', 'body'];
 
     public function replies()
     {
@@ -21,9 +21,17 @@ class Thread extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public function channel(){
+        return $this->belongsTo(Channel::class);
+    }
     // addReply method to be called on thread instance to create reply
     public function addReply(array $reply)
     {   // reply will be created for this relationship
         $this->replies()->create($reply);
+    }
+    // make url for specific model
+    public function path()
+    {
+        return "/threads/{$this->channel->slug}/{$this->id}";
     }
 }

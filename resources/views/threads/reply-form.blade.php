e8e86d566c08d1f9799715129aaa9fde1428cd98
kole <misterija@gmail.com>
2023-08-16 12:14:41 +0200

    added error handler for messeges

--- resources/views/threads/reply-form.blade.php
+++ resources/views/threads/reply-form.blade.php
@@ -1,12 +1,16 @@
 @if(auth()->check())
-<div class="border">
-    <form action="{{ route('threads-reply.store', $thread) }}" method="POST">
+<div class="border p-6 text-gray-900 dark:text-gray-100">
+    <form action="{{ route('threads-reply.store', [$thread->channel, $thread]) }}" method="POST">
         @csrf
         <div class="form-group">
             <textarea name="body" id="body" class="form-control w-full" placeholder="Have something to say?" rows="2"></textarea>
         </div>
         <button type="submit" class="btn btn-primary border p-2">Post</button>
     </form>
+@if ($errors->has('body'))
+    <span class="error">{{ $errors->first('body') }}</span>
+@endif
+
 </div>
 @else
     <p class="text-center">Please <a class="text-orange-200" href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>

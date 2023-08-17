20bce5390bff51e483e362b0abf579c4b2393472
kole <misterija@gmail.com>
2023-08-17 11:51:52 +0200

    add data as composer, not as shared so it could be testable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \View::composer('*', function ($view) {
+            $view->with('channels', \App\Models\Channel::all());
+        });
+        // will be shared SQL query before DB migrations and it will not be visible and it will throw error on testing 
+        // \View::share('channels', \App\Models\Channel::all()); 
     }
 }

6a1ebea772bb02ca664372f38736107974de6c8a
kole <misterija@gmail.com>
2023-08-17 10:36:48 +0200

    Unit test to confirm that thread belongsTo channel relationship

---
+++ tests/Unit/ChannelTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+// use PHPUnit\Framework\TestCase;
+
+use App\Models\Channel;
+use App\Models\Thread;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ChannelTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_a_channel_consist_of_threads()
+    {
+        $channel = factoryCreate(Channel::class);
+        $thread = factoryCreate(Thread::class, ['channel_id' => $channel->id]);
+
+        $this->assertTrue($channel->threads->contains($thread));
+    }
+}

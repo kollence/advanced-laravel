97fd87e265f7da061873b2592dc38a035d1833cc
kole <misterija@gmail.com>
2023-08-18 13:53:05 +0200

    css

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -1,5 +1,5 @@
 <style>
-    .error{
+    .error {
         color: red;
     }
 </style>
@@ -9,42 +9,44 @@
             {{ __('Forum Threads') }}
         </h2>
     </x-slot>
-
-<form method="POST" action="{{ route('threads.store') }}">
-        @csrf
-        <select name="channel_id" id="channels"  class="bg-gray-500 my-3 w-full" required>
-            <option value="">Select a channel</option>
-            @foreach ($channels as $channel)
-                <option value="{{ $channel->id }}" {{old('channel_id') == $channel->id ? 'selected' : ''}} >{{ $channel->name }}</option>
-            @endforeach 
-        </select>
-        @if ($errors->has('channel_id'))
+    <div class="max-w-7xl mx-auto">
+        <form method="POST" action="{{ route('threads.store') }}">
+            @csrf
+            <select name="channel_id" id="channels" class="bg-gray-500 my-3 w-full" required>
+                <option value="">Select a channel</option>
+                @foreach ($channels as $channel)
+                <option value="{{ $channel->id }}" {{old('channel_id') == $channel->id ? 'selected' : ''}}>{{ $channel->name }}</option>
+                @endforeach
+            </select>
+            @if ($errors->has('channel_id'))
             <span class="error">{{ $errors->first('channel_id') }}</span>
-        @endif
-        <!-- Email Address -->
-        <div>
-            <label for="title">
-                <input value="{{old('title')}}" type="text" name="title" id="title" class="bg-gray-500 w-full" required>
-            </label>
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-        @if ($errors->has('title'))
+            @endif
+            <!-- Email Address -->
+            <div>
+                <label for="title">
+                    <input value="{{old('title')}}" type="text" name="title" id="title" class="bg-gray-500 w-full" required>
+                </label>
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+            @if ($errors->has('title'))
             <span class="error">{{ $errors->first('title') }}</span>
-        @endif
-
-        <!-- Password -->
-        <div class="mt-4">
-            <label for="body">
-                <textarea name="body" id="body" cols="30" rows="10" class="bg-gray-500 w-full" required>{{old('body')}}</textarea>
-            </label>
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-        @if ($errors->has('body'))
+            @endif
+    
+            <!-- Password -->
+            <div class="mt-4">
+                <label for="body">
+                    <textarea name="body" id="body" cols="30" rows="10" class="bg-gray-500 w-full" required>{{old('body')}}</textarea>
+                </label>
+    
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+            @if ($errors->has('body'))
             <span class="error">{{ $errors->first('body') }}</span>
-        @endif
+            @endif
+    
+            <button class="border rounded-md border-2 border-gray-500/100 text-white p-2" type="submit">Submit</button>
+    
+        </form>
 
-        <button class="border rounded-md border-2 border-gray-500/100 text-white p-2"  type="submit">Submit</button>
-
-    </form>
+    </div>
 </x-guest-layout>

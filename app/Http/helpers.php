0d191d66c37840e70fa901c9bbf455f3fe4b85e9
kole <misterija@gmail.com>
2023-08-19 14:56:05 +0200

    quantity pass for number of factories

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,16 +1,17 @@
 <?php
 
-function factoryCreate($class, $attributes = [])
+function factoryCreate($class, $attributes = [], $quantity = null)
 {    
-    if(!empty($attributes)){
-        return $class::factory()->create($attributes);
+    if($quantity){
+        return $class::factory()->count($quantity)->create($attributes);
     }
-    return $class::factory()->create();
+    return $class::factory()->create($attributes);
 }
-function factoryMake($class, $attributes = [])
+
+function factoryMake($class, $attributes = [], $quantity = null)
 {
-    if(!empty($attributes)){
-        return $class::factory()->make($attributes);
+    if($quantity){
+        return $class::factory()->count($quantity)->make($attributes);
     }
-    return $class::factory()->make();
+    return $class::factory()->make($attributes);
 }

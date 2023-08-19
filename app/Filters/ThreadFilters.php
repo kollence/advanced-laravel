f01fb170e4d7437e8eeb7852937b5a3e9c7cae80
kole <misterija@gmail.com>
2023-08-19 14:55:05 +0200

    filter for popular & clear query if orderBy exists before

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -6,7 +6,7 @@ use App\Models\User;
 
 class ThreadFilters extends Filters
 {
-    protected $filters = ['by'];
+    protected $filters = ['by', 'popular'];
     /**
      * Filter query by a given usernam
      * 
@@ -17,5 +17,11 @@ class ThreadFilters extends Filters
     {
         $user = User::where('name', $username)->firstOrFail();
         return $this->builder->where('user_id', $user->id);
+    }
+
+    public function popular()
+    {
+        $this->builder->getQuery()->orders = [];
+        return $this->builder->orderBy('replies_count', 'desc');
     }
 }

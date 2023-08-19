64553c9db2ceaea265b25879b467805cc2bb40ba
kole <misterija@gmail.com>
2023-08-19 15:04:39 +0200

    separate html for filters included in needed views

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -1,51 +1,5 @@
 <x-guest-layout> 
-<div class="space-x-8 sm:-my-px sm:ml-10 sm:flex pt-4">
-    <x-dropdown align="right" width="48" class="">
-        <x-slot name="trigger">
-            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                <div>Channels</div>
-
-                <div class="ml-1">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>
-                </div>
-            </button>
-        </x-slot>
-
-        <x-slot name="content">  
-            @foreach($channels as $channel)                     
-            <x-dropdown-link :href="route('threads.index', $channel->slug)">
-                {{$channel->slug}}
-            </x-dropdown-link>
-            @endforeach
-        </x-slot>
-    </x-dropdown>
-    <x-dropdown align="right" width="48" class="">
-        <x-slot name="trigger">
-            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                <div>Browse</div>
-
-                <div class="ml-1">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>
-                </div>
-            </button>
-        </x-slot>
-
-        <x-slot name="content">                    
-            <x-dropdown-link :href="route('threads.index')">
-                All threads
-            </x-dropdown-link>
-            @auth
-            <x-dropdown-link :href="route('threads.index', ['by' => auth()->user()->name])">
-                My threads
-            </x-dropdown-link>
-            @endauth
-        </x-slot>
-    </x-dropdown>
-</div> 
+@include('threads.filters')
 <div class="max-w-7xl mx-auto grid grid-cols-1 gap-4">
 
     <div class="px-5 py-12 col-span-2">
@@ -58,14 +12,20 @@
                     @foreach($threads as $thread)
                         <hr>
                         <hr>
-                        <h3 style="font-size: 20px; color: orange;">
+                        <div class="flex  justify-between">
+                        <h3 style="font-size: 20px; color: orange;" class="self-start">
                             <a href="{{$thread->path()}}">{{ $thread->title }}</a>
                         </h3>
-                            <div class="body bg-gray-500 rounded-md p-3" >{{ $thread->body }}</div>
-                            <div>
-                                <small>Created by: {{$thread->user->name}}</small> <br>
-                                <small>commented: {{$thread->replies_count}} {{Str::plural('time', $thread->replies_count)}}</small>
-                            </div>
+                        <strong class="self-end">
+                            <a href="{{$thread->path()}}" class="ml-2 first-of-type:ml-6 text-orange-400">
+                                replies: {{$thread->replies_count}} {{Str::plural('time', $thread->replies_count)}}
+                            </a>
+                        </strong>
+                        </div>
+                        <div class="body bg-gray-500 rounded-md p-3" >{{ $thread->body }}</div>
+                        <div>
+                            <small>Created by: {{$thread->user->name}}</small> <br>        
+                        </div>
                         <br>
 
                     @endforeach

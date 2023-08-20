f2172ea6b68ce40ebae32834fac2906a5105e848
kole <misterija@gmail.com>
2023-08-20 11:25:29 +0200

    guest cant add & auth can add & auth can add just 1

---
+++ tests/Feature/FavoritesTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Reply;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FavoritesTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_guests_can_not_favorite_anything()
+    {
+        $this->withExceptionHandling();
+        $this->post('/replies/1/favorites')
+            ->assertRedirect('/login');
+    }
+    public function test_auth_user_can_favorite_any_reply()
+    {
+        $this->signIn();
+        // create one reply
+        
+        $reply = factoryCreate(\App\Models\Reply::class);
+        $this->post('/replies/'.$reply->id.'/favorites');
+
+        $this->assertCount(1, $reply->favorites);
+    }
+
+
+    public function test_auth_user_can_favorite_reply_only_once()
+    {
+        $this->signIn();
+        // create one reply
+        $reply = factoryCreate(\App\Models\Reply::class);
+        $this->post('/replies/'.$reply->id.'/favorites');
+        $this->post('/replies/'.$reply->id.'/favorites');
+        // dd(\App\Models\Favorite::all());
+        $this->assertCount(1, $reply->favorites);
+    }
+}

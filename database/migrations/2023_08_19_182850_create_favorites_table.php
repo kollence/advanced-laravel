2ea51b8cd36e3606de61e0f14103f559ffce64cb
kole <misterija@gmail.com>
2023-08-20 11:20:13 +0200

    table with unique fields

---
+++ database/migrations/2023_08_19_182850_create_favorites_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('favorite_id');
+            $table->string('favorite_type', 50)->nullable(); 
+            $table->timestamps();
+
+            $table->unique(['user_id', 'favorite_id', 'favorite_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+};

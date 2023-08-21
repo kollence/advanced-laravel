ca43ff2280d110517ca875ae1937b0d9941cd9e7
kole <misterija@gmail.com>
2023-08-21 13:15:28 +0200

    Switch to View composer global/Chace wont work if tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,11 +22,17 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {   // Cache data for 24 hours
-        $channels = \Cache::remember('channels', now()->addHours(24), function () {
-            return \App\Models\Channel::all();
-        });
+    {   // Cache data always
+        // $channels = \Cache::rememberForever('channels', function () {
+        //     return \App\Models\Channel::all();
+        // });
+
         // will be shared SQL query before DB migrations and it will not be visible and it will throw error on testing 
-        \View::share('channels', $channels); 
+        // \View::share('channels', $channels); 
+
+        // TURN ON IF TESTING
+        \View::composer('*', function ($view) {
+            $view->with('channels', \App\Models\Channel::all());
+        });
     }
 }

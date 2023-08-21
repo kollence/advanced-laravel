0c29629e27b54ad55a94984a4f33b6183f280168
kole <misterija@gmail.com>
2023-08-21 09:30:37 +0200

    global scope class to use WITH on relationship

---
+++ app/Models/Scopes/UserScope.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class UserScope implements Scope
+{
+    protected $relationship;
+
+    public function __construct($relationship)
+    {
+        $this->relationship = $relationship;
+    }
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->with($this->relationship);
+    }
+}

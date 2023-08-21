a8032deae0b1380cc6e719f19c597cad1a4b7661
kole <misterija@gmail.com>
2023-08-21 16:48:10 +0200

    check if you can delete if its your for btn in blade

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,5 +1,7 @@
 <x-guest-layout>
-@include('threads.filters')
+@php
+    $canIfitsYours = ($thread->user_id == auth()->user()->id);
+@endphp
     <div class="max-w-7xl mx-auto grid grid-cols-3 gap-4">
 
     <div class="px-5 py-12 col-span-2">
@@ -7,6 +9,20 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-300">
                     <a href="{{url('threads/create')}}">+ Thread</a>
+                    @if($canIfitsYours)
+                    <form method="POST" action="{{ url($thread->path()) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button 
+                        {{ $canIfitsYours ? '' : 'disabled' }}
+                        type="submit" 
+                        class="flex bg-transparent hover:bg-blue-600  font-bold py-2 px-4 rounded-full shadow-md" 
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Delete') }}
+                        </button>
+                    </form>
+                    @endif
                 </div>
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">

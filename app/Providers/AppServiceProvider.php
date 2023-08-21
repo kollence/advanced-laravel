d643e3cfe3c28100b8f99bc2a8ca738e2b938fd1
kole <misterija@gmail.com>
2023-08-21 09:32:00 +0200

    Cache data to be remember in memory

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,11 +22,11 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        \View::composer('*', function ($view) {
-            $view->with('channels', \App\Models\Channel::all());
+    {   // Cache data for 24 hours
+        $channels = \Cache::remember('channels', now()->addHours(24), function () {
+            return \App\Models\Channel::all();
         });
         // will be shared SQL query before DB migrations and it will not be visible and it will throw error on testing 
-        // \View::share('channels', \App\Models\Channel::all()); 
+        \View::share('channels', $channels); 
     }
 }

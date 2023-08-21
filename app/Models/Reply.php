fb4aafd8b66cb8f4e6abd84ff6ecbe650d9f31ae
kole <misterija@gmail.com>
2023-08-21 09:23:24 +0200

    eager loading for user and favorites relations for reply

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -2,37 +2,23 @@
 
 namespace App\Models;
 
+use App\Traits\Favoritable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    use HasFactory;
+    use HasFactory, Favoritable;
 
     protected $guarded = ['id'];
     protected $fillable = ['thread_id', 'user_id', 'body'];
+
+    // always load user with model results
+    protected $with = ['user', 'favorites'];
+
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorite');
-    }
-
-    public function addFavorite()
-    {
-        $this->favorites()->create(['user_id' => auth()->id()]);
-    }
-    public function removeFavorite()
-    {
-        $this->favorites()->where('user_id', auth()->id())->delete();
-    }
-    public function isFavorited()
-    {
-        return (bool) $this->favorites()->where('user_id', auth()->id())->count();
-    }
-
 }

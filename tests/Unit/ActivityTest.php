7177ef804c664e26b37bb255ef61acc47dc72261
kole <misterija@gmail.com>
2023-08-22 16:43:28 +0200

    test create activity when reply is created

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 // use PHPUnit\Framework\TestCase;
 
 use App\Models\Activity;
+use App\Models\Reply;
 use App\Models\Thread;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -32,5 +33,16 @@ class ActivityTest extends TestCase
 
         $activity = Activity::first(); 
         $this->assertEquals($activity->subject->id, $thread->id);
+    }
+
+    public function test_create_activity_when_reply_is_created()
+    {
+        $this->signIn();
+
+        $reply = factoryCreate(Reply::class);
+
+
+        $activity = Activity::count(); 
+        $this->assertEquals(2, $activity);
     }
 }

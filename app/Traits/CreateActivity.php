54e5320fa5c90793dfee38cc5098d3c7d25e42a4
kole <misterija@gmail.com>
2023-08-22 16:05:54 +0200

    boot method from trait convention bootNameOfTrait

--- app/Traits/CreateActivity.php
+++ app/Traits/CreateActivity.php
@@ -6,18 +6,29 @@ use App\Models\Activity;
 
 trait CreateActivity 
 {
+
+    // trait listen when modal is created and it will generate a new activity
+    protected static function bootCreateActivity()
+    {
+        if(auth()->check()){
+            static::created(function ($thread) {
+                $thread->createActivityWhenThreadIsCreated('created');
+            });
+        }
+    }
+
     protected function createActivityWhenThreadIsCreated($event)
     {
         Activity::create([
             'user_id' => auth()->id(),
-            'type' => $event . '.' . $this->getClassNameToLowercase(),
+            'type' => $this->getClassNameToLowercase($event),
             'subject_id' => $this->id,
             'subject_type' => get_class($this)
         ]);
     }
 
-    protected function getClassNameToLowercase()
-    {
-        return strtolower(class_basename($this));
+    protected function getClassNameToLowercase($event)
+    {   $type = strtolower(class_basename($this));
+        return "{$event}.{$type}";
     }
 }

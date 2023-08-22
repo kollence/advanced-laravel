3703cf10f38ec0665975fbe4d06e608971ed710b
kole <misterija@gmail.com>
2023-08-22 14:34:42 +0200

    test create activity when thread is created

---
+++ tests/Unit/ActivityTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+// use PHPUnit\Framework\TestCase;
+
+use App\Models\Thread;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ActivityTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_create_activity_when_thread_is_created()
+    {
+        $this->signIn();
+
+        $thread = factoryCreate(Thread::class);
+
+        $this->assertDatabaseHas('activities', [
+            'user_id' => auth()->id(),
+            'subject_id' => $thread->id,
+            'type' => 'created.thread',
+            'subject_type' => Thread::class,
+        ]);
+    }
+}

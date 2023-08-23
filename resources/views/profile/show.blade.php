95cb5d5834d517993edf67f5c777b92ecd9628b5
kole <misterija@gmail.com>
2023-08-23 10:23:40 +0200

    polimorph in blade

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -14,31 +14,17 @@
             </div>
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="w-full text-white">
-                @foreach($threads as $thread)
-                        <hr>
-                        <hr>
-                        <div class="flex  justify-between">
-                        <h3 style="font-size: 20px; color: orange;" class="self-start">
-                            <a href="{{$thread->path()}}">{{ $thread->title }}</a>
-                        </h3>
-                        <strong class="self-end">
-                            <a href="{{$thread->path()}}" class="ml-2 first-of-type:ml-6 text-orange-400">
-                                replies: {{$thread->replies_count}} {{Str::plural('time', $thread->replies_count)}}
-                            </a>
-                        </strong>
-                        </div>
-                        <div class="body bg-gray-500 rounded-md p-3" >{{ $thread->body }}</div>
-                        <div>
-                            <small>Created by: <a class="text-orange-400" href="{{route('profile.show', $thread->user->name)}}">{{ $thread->user->name }}</a></small> <br>        
-                        </div>
-                        <br>
-
+                    @foreach($activities as $date => $activity)
+                        <h4>{{$date}}</h4>
+                        @foreach($activity as $record)
+                                <!-- polimorph in blade || event.type -->
+                            @include("profile.activities.{$record->type}", ['activity'=> $record])
+                        @endforeach
                     @endforeach
 
                 </div>
-                {{$threads->links()}}
-                </div>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>

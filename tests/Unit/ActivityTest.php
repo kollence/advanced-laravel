0a31848a69665758157b115d179818c747f5d0fb
kole <misterija@gmail.com>
2023-08-23 16:18:49 +0200

    test fetching activity feed for any user

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -7,6 +7,8 @@ namespace Tests\Unit;
 use App\Models\Activity;
 use App\Models\Reply;
 use App\Models\Thread;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -41,8 +43,28 @@ class ActivityTest extends TestCase
 
         $reply = factoryCreate(Reply::class);
 
-
         $activity = Activity::count(); 
         $this->assertEquals(2, $activity);
+    }
+
+    public function test_fetch_feed_for_any_user()
+    {
+        $this->signIn();
+
+        factoryCreate(Thread::class, ['user_id' => auth()->id()], 2);
+
+        $user = User::find(auth()->id());
+
+        $user->activity()->first()->update(['created_at' => Carbon::now()->subWeek()]);
+ 
+        $feed = Activity::feed($user);
+        
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->format('Y-m-d')
+        ));
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->subWeek()->format('Y-m-d')
+        ));
+        
     }
 }

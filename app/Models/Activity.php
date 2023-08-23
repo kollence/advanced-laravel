dde7b7a6c193d10f7a6c527ee2758ce19c900b80
kole <misterija@gmail.com>
2023-08-23 16:17:47 +0200

    feed method in modal with static::where

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -14,10 +14,22 @@ class Activity extends Model
         'user_id' ,
         'subject_id' ,
         'subject_type' ,
+        'created_at' ,
     ];
 
     public function subject()
     {
         return $this->morphTo();
+    }
+
+    public static function feed($user, $amount = 20)
+    {
+        // return $user->activity()
+        return static::where('user_id', $user->id)
+            ->latest()
+            ->with('subject')
+            ->take($amount)
+            ->get()
+            ->groupBy(fn ($activity) => $activity->created_at->format('Y-m-d'));
     }
 }

8419e29a80f9e9c09d4c01205022410b4a9e5043
kole <misterija@gmail.com>
2023-08-23 11:09:31 +0200

    relationship user->activity & replay->thread

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->email === 'misterija@gmail.com';
     }
+
+    public function activity()
+    {
+        return $this->hasMany(Activity::class);
+    }
 }

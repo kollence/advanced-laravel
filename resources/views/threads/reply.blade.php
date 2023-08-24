c884fb18eb0da64d525901754fd830874781a978
kole <misterija@gmail.com>
2023-08-24 17:03:36 +0200

    path() to thread with scroll to id of replay

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,4 +1,4 @@
-<div class="p-6 text-gray-900 dark:text-gray-100 ">
+<div id="reply-{{$reply->id}}" class="p-6 text-gray-900 dark:text-gray-100 ">
     <h5 style="font-size: 20px;">
         <a style=" color: orange;" href="{{route('profile.show', $reply->user->id)}}">{{ $reply->user->name }}</a>
         said...

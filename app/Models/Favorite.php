de0ced77da1b3af3bfe55eb0483e5b8bfd9ab9c5
kole <misterija@gmail.com>
2023-08-24 17:02:37 +0200

    Favorite will create activity

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
+use App\Traits\CreateActivity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
-    use HasFactory;
+    use HasFactory, CreateActivity;
 
     protected $fillable = ['user_id', 'favorite_id', 'favorite_type'];
 
+    public function favorite()
+    {
+        return $this->morphTo();
+    }
+    
 }

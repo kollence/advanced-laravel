cb465105ad907c8eb270826f14a1d73003656b38
kole <misterija@gmail.com>
2023-08-29 11:52:15 +0200

    controller index for thread replies with paginate

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -18,9 +18,9 @@ class ReplyController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Channel $channel, Thread $thread)
     {
-        //
+        return $thread->replies()->paginate(10);
     }
 
     /**

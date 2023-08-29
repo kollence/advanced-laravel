5bdaaf62d44bb637a63ea373b549533ed63019fa
kole <misterija@gmail.com>
2023-08-29 15:49:27 +0200

    test fix for added field replies_count

--- tests/Feature/ParticipateInThreadsTest.php
+++ tests/Feature/ParticipateInThreadsTest.php
@@ -17,19 +17,21 @@ class ParticipateInThreadsTest extends TestCase
      *
      * @return void
      */
-    public function test_unauthenticated_user_cant_add_reply()
+    public function test_guest_cant_add_reply()
     {
         $this->post('/threads/some-channel/1/replies', [])->assertRedirect('/login');
     }
 
-    public function test_authenticated_user_can_add_reply()
+    public function test_auth_can_add_reply()
     { 
         $this->signIn();
 
         $thread = factoryCreate(Thread::class);
         $reply = factoryMake(Reply::class);
         $this->post($thread->path().'/replies', $reply->toArray());
-        $this->get($thread->path())->assertSee($reply->body);
+        // $this->get($thread->path())->assertSee($reply->body);
+        $this->assertDatabaseHas('replies', ['body' => $reply->body]);
+        $this->assertEquals(1, $thread->fresh()->replies_count);
     }
 
     public function test_a_reply_requiers_a_body()
@@ -41,43 +43,44 @@ class ParticipateInThreadsTest extends TestCase
         ->assertSessionHasErrors('body');
     }
 
-    public function test_unauthorized_user_cant_delete_reply()
+    public function test_guest_cant_delete_reply()
     {
         $this->withExceptionHandling();
         $reply = factoryCreate(Reply::class);
         $this->delete("/replies/{$reply->id}")->assertRedirect('/login');
     }
 
-    public function test_authorized_user_cant_delete_reply_that_its_not_its_own()
+    public function test_auth_cant_delete_reply_that_its_not_its_own()
     {
         $this->signIn();
         $reply = factoryCreate(Reply::class);
         $this->delete("/replies/{$reply->id}")->assertStatus(403); // 403 Forbidden
     }
 
-    public function test_authorized_user_can_delete_only_its_own_reply()
+    public function test_auth_can_delete_only_its_own_reply()
     {
         $this->signIn();
         $reply = factoryCreate(Reply::class, ['user_id' => auth()->id()]);
         $this->delete("/replies/{$reply->id}")->assertStatus(302);
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
+        $this->assertEquals(0, $reply->thread->fresh()->replies_count);
     }
 
-    public function test_unauthorized_user_cant_update_reply()
+    public function test_guest_cant_update_reply()
     {
         $this->withExceptionHandling();
         $reply = factoryCreate(Reply::class);
         $this->patch("/replies/{$reply->id}")->assertRedirect('/login');
     }
 
-    public function test_authorized_user_cant_update_reply_that_its_not_its_own()
+    public function test_auth_cant_update_reply_that_its_not_its_own()
     {
         $this->signIn();
         $reply = factoryCreate(Reply::class);
         $this->patch("/replies/{$reply->id}")->assertStatus(403); // 403 Forbidden
     }
 
-    public function test_authorized_user_can_update_only_its_own_reply()
+    public function test_auth_can_update_only_its_own_reply()
     {
         $this->signIn();
         $reply = factoryCreate(Reply::class, ['user_id' => auth()->id()]);

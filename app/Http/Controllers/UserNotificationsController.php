297bba8280a0e0fc8d840d6a4b7b67b61c665b1a
kole <misterija@gmail.com>
2023-08-31 16:04:50 +0200

    index. all unreadNotifications that are from auth user

--- app/Http/Controllers/UserNotificationsController.php
+++ app/Http/Controllers/UserNotificationsController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 
 class UserNotificationsController extends Controller
 {
+
+    public function index(User $user)
+    {
+        $notifications = auth()->user()->unreadNotifications()->paginate(10);
+        return $notifications;
+        // return view('user.notifications.index', compact('notifications'));
+    }
+
+
     public function destroy(User $user, $notificationId)
     {                                                // mark as read update column read_at in notification table
         auth()->user()->notifications()->findOrFail($notificationId)->markAsRead();

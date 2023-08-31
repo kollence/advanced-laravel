cd2b1be4af73f701ae3ee0aac83f0574eba47779
kole <misterija@gmail.com>
2023-08-31 16:01:28 +0200

    route get index

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::get('/profile/{user}/notifications', [UserNotificationsController::class, 'index'])->name('profile.notifications.index');
     Route::delete('/profile/{user}/notifications/{notification}', [UserNotificationsController::class, 'destroy'])->name('profile.notifications.delete');
 });
 

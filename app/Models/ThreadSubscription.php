5f2897ae0b717fef656078e2a026a1de99eceda6
kole <misterija@gmail.com>
2023-08-31 14:49:24 +0200

    ThredSubscription notfy method to extract logic

--- app/Models/ThreadSubscription.php
+++ app/Models/ThreadSubscription.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\ThreadWasReplied;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,5 +15,10 @@ class ThreadSubscription extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function notify($reply)
+    {
+        $this->user->notify(new ThreadWasReplied($this, $reply));
     }
 }

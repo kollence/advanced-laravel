1dd2513e1b6174388b57a93f098f4b0c28400b77
kole <misterija@gmail.com>
2023-08-31 15:33:14 +0200

    test auth can clear notification

--- tests/Feature/NotificationTest.php
+++ tests/Feature/NotificationTest.php
@@ -17,26 +17,44 @@ class NotificationTest extends TestCase
      */
     public function test_notification_is_prepared_when_subscribed_thred_got_a_new_reply_that_is_not_created_by_current_subscribed_user()
     {
-                //sign in
-                $this->signIn();
-                //create thread
-                $thread = factoryCreate(\App\Models\Thread::class)->subscribe();
-                // check that subscribed thread doesn't have a notification
-                $this->assertCount(0, auth()->user()->notifications);
-                // reply created by me
-                $thread->addReply([
-                    'user_id' => auth()->id(),
-                    'body' => 'My reply will not notify me'
-                ]);
-                // make sure that notification is not created for current subscribed user
-                $this->assertCount(0, auth()->user()->fresh()->notifications);
-                // reply created from other user
-                $thread->addReply([
-                    'user_id' => factoryCreate(\App\Models\User::class)->id,
-                    'body' => 'Others users reply will notify me'
-                ]);
-                // reply from others will notify me
-                $this->assertCount(1, auth()->user()->fresh()->notifications);
+        //sign in
+        $this->signIn();
+        //create thread
+        $thread = factoryCreate(\App\Models\Thread::class)->subscribe();
+        // check that subscribed thread doesn't have a notification
+        $this->assertCount(0, auth()->user()->notifications);
+        // reply created by me
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'My reply will not notify me'
+        ]);
+        // make sure that notification is not created for current subscribed user
+        $this->assertCount(0, auth()->user()->fresh()->notifications);
+        // reply created from other user
+        $thread->addReply([
+            'user_id' => factoryCreate(\App\Models\User::class)->id,
+            'body' => 'Others users reply will notify me'
+        ]);
+        // reply from others will notify me
+        $this->assertCount(1, auth()->user()->fresh()->notifications);
+    }
 
+    public function test_an_auth_can_clear_a_notification()
+    {
+        $this->signIn();
+        $user = auth()->user();
+        $thread = factoryCreate(\App\Models\Thread::class)->subscribe();
+
+        $thread->addReply([
+            'user_id' => factoryCreate(\App\Models\User::class)->id,
+            'body' => 'Others users reply will notify me'
+        ]);
+
+        $this->assertCount(1, $user->unreadNotifications);
+
+        
+        $this->delete(route('profile.notifications.delete', [$user->name, $user->unreadNotifications->first()->id]));
+
+        $this->assertCount(0, $user->fresh()->unreadNotifications);
     }
 }

a098d98230b1b011fcfe1bed1fa28597b7260076
kole <misterija@gmail.com>
2023-09-01 17:31:24 +0200

    test refactory & calling DatabaseNotificationFactory directly

--- tests/Feature/NotificationTest.php
+++ tests/Feature/NotificationTest.php
@@ -2,23 +2,27 @@
 
 namespace Tests\Feature;
 
+use Database\Factories\DatabaseNotificationFactory;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class NotificationTest extends TestCase
 {
     use DatabaseMigrations;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->signIn();
+    }
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_notification_is_prepared_when_subscribed_thred_got_a_new_reply_that_is_not_created_by_current_subscribed_user()
+    public function test_notification_is_prepared_when_subscribed_thread_got_a_new_reply_that_is_not_created_by_current_subscribed_user()
     {
-        //sign in
-        $this->signIn();
         //create thread
         $thread = factoryCreate(\App\Models\Thread::class)->subscribe();
         // check that subscribed thread doesn't have a notification
@@ -41,37 +45,24 @@ class NotificationTest extends TestCase
 
     public function test_auth_can_fetch_their_unread_notifications()
     {
-        $this->signIn();
+        // FACTORY call for generated Laravels Model Illuminate\Notifications\DatabaseNotification
+        // create a notification
+        DatabaseNotificationFactory::new()->create();
 
-        $user = auth()->user();
-
-        $thread = factoryCreate(\App\Models\Thread::class)->subscribe();
-
-        $thread->addReply([
-            'user_id' => factoryCreate(\App\Models\User::class)->id,
-            'body' => 'Others users reply will notify me'
-        ]);
-
-        $response = 
-            $this->getJson(route('profile.notifications.index', $user->name))
-                ->assertOk();
-        dd($response->json()['data']);
+        $response = $this->getJson(route('profile.notifications.index', auth()->user()))
+                    ->assertOk();
+        
         $this->assertCount(1, $response->json()['data']);
     }
 
     public function test_an_auth_can_mark_a_notification_as_read()
     {
-        $this->signIn();
         $user = auth()->user();
-        $thread = factoryCreate(\App\Models\Thread::class)->subscribe();
-
-        $thread->addReply([
-            'user_id' => factoryCreate(\App\Models\User::class)->id,
-            'body' => 'Others users reply will notify me'
-        ]);
+        // FACTORY call for generated Laravels Model Illuminate\Notifications\DatabaseNotification
+        // create a notification
+        DatabaseNotificationFactory::new()->create();
 
         $this->assertCount(1, $user->unreadNotifications);
-
         
         $this->delete(route('profile.notifications.delete', [$user->name, $user->unreadNotifications->first()->id]));
 

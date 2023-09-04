bcb836b6f095a826f1d43f1fbd80c02ed4d09781
kole <misterija@gmail.com>
2023-09-04 16:20:23 +0200

    artisan event:generate & automatically add events and listeners

---
+++ app/Listeners/SendNotificationsToThreadSubscribers.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ThreadHasNewReply;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendNotificationsToThreadSubscribers
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\ThreadHasNewReply  $event
+     * @return void
+     */
+    public function handle(ThreadHasNewReply $event)
+    {
+        //
+    }
+}

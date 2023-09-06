577c87a59e9f162a115337563dedeae2b5e26bd3
kole <misterija@gmail.com>
2023-09-06 20:18:07 +0200

    html blade error field

--- resources/views/threads/reply-form.blade.php
+++ resources/views/threads/reply-form.blade.php
@@ -1,5 +1,8 @@
 @auth
 <div class="py-4 px-8 text-gray-900 dark:text-gray-100">
+    @if ($errors->has('body'))
+        <span class="text-red-300">{{ $errors->first('body') }}</span>
+    @endif
     <form action="{{ route('threads-reply.store', [$thread->channel, $thread]) }}" method="POST">
         @csrf
         <div class="form-group">
@@ -7,10 +10,6 @@
         </div>
         <button type="submit" class="bg-slate-700 text-sm border rounded-md p-1">Reply</button>
     </form>
-@if ($errors->has('body'))
-    <span class="error">{{ $errors->first('body') }}</span>
-@endif
-
 </div>
 @else
     <p class="text-center">Please <a class="text-orange-200" href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>

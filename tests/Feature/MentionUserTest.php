17d862eecb53f660dd36f86dc0f6451e99a1f4ed
kole <misterija@gmail.com>
2023-09-12 14:33:31 +0200

    test tag a user in reply text and notify tagged user

---
+++ tests/Feature/MentionUserTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Reply;
+use App\Models\Thread;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MentionUserTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_mentioned_user_in_a_reply_are_notified()
+    {
+        //Given we have user JohnDoe who is sign in
+        //And another user JaneDoe who is not sign in
+
+        $john = factoryCreate(User::class,['name'=>'JohnDoe']);
+        $jane = factoryCreate(User::class,['name'=>'JaneDoe']);
+        $this->signIn($john);
+        //We have thread
+        $thread = factoryCreate(Thread::class);
+        //And JohnDoe send a reply and mention JaneDoe and one more user
+        $reply = factoryMake(Reply::class,['body'=>'@JaneDoe look at this thread and you too @SecondUser']);
+        $this->post($thread->path().'/replies',$reply->toArray());
+
+        //Then, JaneDoe should be notified
+        $this->assertCount(1,$jane->notifications);
+    }
+}

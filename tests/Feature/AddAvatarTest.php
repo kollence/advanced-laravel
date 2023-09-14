6336c44310815554314e8f2ea2d3a0ad71153b2b
kole <misterija@gmail.com>
2023-09-14 16:10:29 +0200

    test guestCantAdd |authCantAddInvalid |authCanAddValid

--- tests/Feature/AddAvatarTest.php
+++ tests/Feature/AddAvatarTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class AddAvatarTest extends TestCase
@@ -11,10 +13,30 @@ class AddAvatarTest extends TestCase
      *
      * @return void
      */
-    // public function test_guset_cant_add_avatar_image()
-    // {
-    //     // $this->signIn();
+    public function testGusetCantAddAvatarImage()
+    {
+        $this->json('post', 'api/users/1/avatar')->assertStatus(401);
+    }
 
+    public function testAuthCantAddInvalidAvatarImage()
+    {
+        $this->signIn();
+        $this->json('post', 'api/users/'.auth()->id().'/avatar', [
+            'avatar_img' => 'invalid-image'
+        ])->assertStatus(422);
+    }
 
-    // }
+    public function testAuthCanAddValidAvatarImage()
+    {
+        $this->signIn();
+        Storage::fake('public');
+        $file = UploadedFile::fake()->image('avatar.jpg', 30,30);
+        $this->json('POST', 'api/users/'.auth()->id().'/avatar', [
+            'avatar_img' => $file
+        ]);
+        // Assert that path is saved to users table
+        $this->assertEquals('avatars/'.$file->hashName(), auth()->user()->avatar_img);
+        // Assert the file was stored...
+        Storage::disk('public')->assertExists('avatars/'. $file->hashName());
+    }
 }

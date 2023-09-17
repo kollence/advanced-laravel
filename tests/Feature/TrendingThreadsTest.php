fd78e6bf51d8a45b9d8315a6455784fb42201657
kole <misterija@gmail.com>
2023-09-17 17:42:29 +0200

    test add TrendingThreads| flush redis on init & on end

--- tests/Feature/TrendingThreadsTest.php
+++ tests/Feature/TrendingThreadsTest.php
@@ -3,13 +3,21 @@
 namespace Tests\Feature;
 
 use App\Models\Thread;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Redis\TrendingThreads;
 use Illuminate\Support\Facades\Redis;
 use Tests\TestCase;
 
 class TrendingThreadsTest extends TestCase
 {
+    protected $trendingThreads;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->trendingThreads = new TrendingThreads();
+        //delete if its exist
+        $this->trendingThreads->flush();
+    }
     /**
      * A basic feature test example.
      *
@@ -17,20 +25,27 @@ class TrendingThreadsTest extends TestCase
      */
     public function test_increment_a_threads_score_each_time_it_is_read()
     {   
-        //delete if its exist
-        Redis::del('trending_treads');
         //no one red the thread and count its 0
-        $this->assertEmpty(Redis::zrevrange('trending_treads', 0, -1));
+        $this->assertEmpty($this->trendingThreads->take());
 
         $thread = factoryCreate(Thread::class);
         //go to thread and show it to me so i can read
         $this->get($thread->path())->assertSee($thread->title);
         //thread is read and count is 1
-        $this->assertCount(1, Redis::zrevrange('trending_treads', 0, -1));
+        $this->assertCount(1, $this->trendingThreads->take());
         
-        $redisTrendingThread = Redis::zrevrange('trending_treads', 0, -1);
+        $redisTrendingThread = $this->trendingThreads->take();
         // compare returned result from redis with the thread title
-        $this->assertEquals($thread->title, json_decode($redisTrendingThread[0])->title);
+        $this->assertEquals($thread->title, $redisTrendingThread[0]->title);
 
+    }
+
+    public function test_it_can_cleanup_redis_after_testing()
+    {
+        // Delete the test key from Redis
+        $this->trendingThreads->flush();
+
+        // Assert that the key no longer exists in Redis
+        $this->assertNull(Redis::get('testing_trending_treads'));
     }
 }

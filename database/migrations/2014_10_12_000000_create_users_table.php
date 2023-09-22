ffd29339b20cf88f7741ae20f30d2023ea0b578d
kole <misterija@gmail.com>
2023-09-22 15:35:09 +0200

    DB users migratin| add confirmation_token field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('confirmed_email')->default(false);
+            $table->string('confirmation_token')->nullable();
             $table->string('avatar_img')->nullable();
             $table->rememberToken();
             $table->timestamps();

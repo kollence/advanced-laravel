7fcbbdd542695d396d59950d767edf4695c08c89
kole <misterija@gmail.com>
2023-09-28 10:33:08 +0200

    PROBLEM with SQLite in this case on DB level

--- tests/Feature/MarkBestReplyTest.php
+++ tests/Feature/MarkBestReplyTest.php
@@ -46,4 +46,24 @@ class MarkBestReplyTest extends TestCase
 
         $this->post(route('mark-best-reply.store', $replies[1]->fresh()->id))->assertStatus(403);
     }
+
+    // // THIS TEST CAN NOT BE USED IN TESTING BECAUSE OF PROBLEM WITH DELETING ON TABLE MIGRATION LVL WITH SQLITE FOREIGN KEY
+    // // WHEN REPLY IS DELETED THEN THE BEST_REPLY_ID IS NOT SET ON NULL FROM THE THREADS TABLE
+    // // BUT IT WORKS IN DEVELOPMENT ENVIRONMENT.
+    // public function test_when_best_reply_is_deleted_then_delete_threads_best_reply_id()
+    // {
+    //     $this->signIn();
+
+    //     $thread = factoryCreate(\App\Models\Thread::class, ['user_id' => auth()->id()]);
+
+    //     $replies = factoryCreate(\App\Models\Reply::class, ['thread_id' =>$thread->id], 2);
+
+    //     $this->post(route('mark-best-reply.store', $replies[1]->id));
+
+    //     $this->assertTrue($replies[1]->fresh()->isBestReply());
+
+    //     $replies[1]->delete();
+    //     // dd(\App\Models\Reply::all('id'));
+    //     $this->assertNull($thread->fresh()->best_reply_id);
+    // }
 }

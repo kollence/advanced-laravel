b4ef67a1965264e6063d527310fc9a8c8abfa682
kole <misterija@gmail.com>
2023-09-28 10:34:54 +0200

    TestCase sign in with confirmed email

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,14 +4,31 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Schema;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, DatabaseMigrations;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Schema::enableForeignKeyConstraints();       
+    }
+    
     protected function signIn($user = null)
     {
         $user = $user ?: factoryCreate(\App\Models\User::class);
+
+        $this->actingAs($user);
+
+        return $this;
+    }
+
+    protected function signInConfirmedUser($user = null)
+    {
+        $user = $user ?: factoryCreate(\App\Models\User::class, ['confirmed_email' => true]);
 
         $this->actingAs($user);
 

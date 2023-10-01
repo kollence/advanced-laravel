f3e1b432a15e63450ebf3e112f424a4e424c314a
kole <misterija@gmail.com>
2023-10-01 18:54:29 +0200

    test once locked thread can't receive reply

--- tests/Feature/LockThreadTest.php
+++ tests/Feature/LockThreadTest.php
@@ -17,7 +17,7 @@ class LockThreadTest extends TestCase
     {
         $this->signIn();
 
-        $thread = factoryCreate(\App\Models\Thread::class, ['user_id' => auth()->id()]);
+        $thread = factoryCreate(\App\Models\Thread::class, ['user_id' => auth()->id(), 'locked' => false]);
 
         $this->post($thread->path() . '/lock')->assertStatus(403);
 
@@ -27,18 +27,30 @@ class LockThreadTest extends TestCase
     public function test_admin_can_lock_any_thread()
     {
         $this->signInAsAdmin();
-        $thread = factoryCreate(\App\Models\Thread::class, ['user_id' => auth()->id()]);
+        $thread = factoryCreate(\App\Models\Thread::class, ['user_id' => auth()->id(),'locked' => false]);
 
         $this->post($thread->path() . '/lock');
 
         $this->assertTrue((bool) $thread->fresh()->locked);
     }
 
+    public function test_once_locked_thread_cannot_receive_new_replies()
+    {
+        $this->signInAsAdmin();
+        $thread = factoryCreate(\App\Models\Thread::class, ['user_id' => auth()->id(),'locked' => false]);
+        $this->post($thread->path() . '/lock');
+        $this->assertTrue((bool) $thread->fresh()->locked);
+
+        $reply = factoryCreate(\App\Models\Reply::class, ['thread_id' => $thread->id]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())->assertStatus(422);
+    }
+
     public function test_non_admin_cannot_unlock_thread()
     {
         $this->signInAsAdmin();
         
-        $thread = factoryCreate(\App\Models\Thread::class, ['user_id' => auth()->id()]);
+        $thread = factoryCreate(\App\Models\Thread::class, ['user_id' => auth()->id(),'locked' => false]);
 
         $this->post($thread->path() . '/lock');
 
@@ -55,7 +67,7 @@ class LockThreadTest extends TestCase
     {
         $this->signInAsAdmin();
 
-        $thread = factoryCreate(\App\Models\Thread::class, ['user_id' => auth()->id()]);
+        $thread = factoryCreate(\App\Models\Thread::class, ['user_id' => auth()->id(), 'locked' => true]);
 
         $this->post($thread->path() . '/unlock');
 

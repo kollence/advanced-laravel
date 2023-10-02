3bd1d4a0636029dbd13ab1e99c0291f42b990302
kole <misterija@gmail.com>
2023-10-02 17:29:44 +0200

    create| add attributes to btn & script for recaptcha

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -1,19 +1,20 @@
+
 <style>
     .error {
         color: red;
     }
 </style>
-<x-guest-layout>
+<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Forum Threads') }}
         </h2>
     </x-slot>
     <div class="max-w-7xl mx-auto">
-        <form method="POST" action="{{ route('threads.store') }}">
+        <form method="POST" action="{{ route('threads.store') }}" id="create-thread">
             @csrf
             @if ($errors->has('channel_id'))
-                <span class="error">{{ $errors->first('channel_id') }}</span>
+            <span class="error">{{ $errors->first('channel_id') }}</span>
             @endif
             <select name="channel_id" id="channels" class="bg-gray-500 my-3 w-full" required>
                 <option value="">Select a channel</option>
@@ -25,29 +26,39 @@
             <!-- Email Address -->
             <div>
                 @if ($errors->has('title'))
-                    <span class="error">{{ $errors->first('title') }}</span>
+                <span class="error">{{ $errors->first('title') }}</span>
                 @endif
                 <label for="title">
                     <input value="{{old('title')}}" type="text" name="title" id="title" class="bg-gray-500 w-full" required>
                 </label>
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
-    
+
             <!-- Password -->
             <div class="mt-4">
                 @if ($errors->has('body'))
-                    <span class="error">{{ $errors->first('body') }}</span>
+                <span class="error">{{ $errors->first('body') }}</span>
                 @endif
                 <label for="body">
                     <textarea name="body" id="body" cols="30" rows="10" class="bg-gray-500 w-full" required>{{old('body')}}</textarea>
                 </label>
-    
+
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
-    
-            <button class="border rounded-md border-2 border-gray-500/100 text-white p-2" type="submit">Submit</button>
-    
-        </form>
 
+            <button class="g-recaptcha border rounded-md border-2 border-gray-500/100 text-white p-2" type="submit"
+                data-sitekey="{{config('services.recaptcha.sitekey')}}" 
+                data-callback='onSubmit' 
+                data-action='submit'
+            >Submit</button>
+
+        </form>
     </div>
-</x-guest-layout>
+    @push('scripts')
+    <script>
+        function onSubmit(token) {
+            document.getElementById("create-thread").submit();
+        }
+    </script>
+    @endpush
+</x-app-layout>

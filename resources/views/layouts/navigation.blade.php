26ab14ab0c489ebecbfbce2f383bde24d3f3b438
kole <misterija@gmail.com>
2023-10-09 17:46:09 +0200

    remove fiters as menu&move to checkbox section

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -43,38 +43,6 @@
                             </x-slot>
                         </x-dropdown>
                     </div>
-                    <div class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
-                        <x-dropdown align="left" width="48" height="30">
-                            <x-slot name="trigger">
-                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                                    <div>Browse</div>
-
-                                    <div class="ml-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
-
-                            <x-slot name="content">
-                                <x-dropdown-link :href="route('threads.index')">
-                                    All threads
-                                </x-dropdown-link>
-                                @auth
-                                <x-dropdown-link :href="route('threads.index', ['by' => auth()->user()->name])">
-                                    My threads
-                                </x-dropdown-link>
-                                @endauth
-                                <x-dropdown-link :href="route('threads.index', ['popular' => 1])">
-                                    Popular
-                                </x-dropdown-link>
-                                <x-dropdown-link :href="route('threads.index', ['unanswered' => 1])">
-                                    Unanswered
-                                </x-dropdown-link>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
                 </div>
             </div>
             @auth

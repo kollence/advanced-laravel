3fe42872e52707cdfd9a9b19282b33e967251f70
kole <misterija@gmail.com>
2023-11-11 20:16:52 +0100

    Controller ContestEmails| index & store

---
+++ app/Http/Controllers/ContestEmailsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\NewContestEmailReceivedEvent;
+use App\Models\ContestEmails;
+use Illuminate\Http\Request;
+
+class ContestEmailsController extends Controller
+{
+    public function index(){
+        return view('contest.index');
+    }
+
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+                'email' => 'required|email',
+        ]);
+
+        ContestEmails::create($data);
+
+        // event(NewContestEmailReceivedEvent::class);
+    }
+}

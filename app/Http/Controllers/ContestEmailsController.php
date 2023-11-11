26dcb31c7347d8dc8949d41a341a4c99af4cccc0
kole <misterija@gmail.com>
2023-11-11 20:46:43 +0100

    or remove Trigger Event in modal events| trigger when created

--- app/Http/Controllers/ContestEmailsController.php
+++ app/Http/Controllers/ContestEmailsController.php
@@ -21,7 +21,5 @@ class ContestEmailsController extends Controller
 
         ContestEmails::create($data);
 
-        // event(NewContestEmailReceivedEvent::class);
-        NewContestEmailReceivedEvent::dispatch();
     }
 }

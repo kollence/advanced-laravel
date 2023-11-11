1faaec920f9fb6d101d8852538d67a440819864d
kole <misterija@gmail.com>
2023-11-11 15:40:59 +0100

    New Route grouping by controller

--- routes/web.php
+++ routes/web.php
@@ -32,30 +32,38 @@ Route::get('/', function () {
 Route::get('/register/confirm', [RegisteredUserController::class, 'confirm'])
 ->name('register.confirm');
 //write route for threads with optional channel param
-Route::get('threads/create', [ThreadController::class, 'create'])->name('threads.create');
-Route::get('threads/{thread}/edit', [ThreadController::class, 'edit'])->name('threads.edit');
-Route::patch('threads/{thread}/update', [ThreadController::class, 'update'])->name('threads.update');
-Route::get('threads/{channel}/{thread}', [ThreadController::class, 'show'])->name('threads.show');
-Route::post('threads', [ThreadController::class, 'store'])->name('threads.store')->middleware(ConfirmedEmailCanCreate::class);
-Route::get('threads/{channel?}', [ThreadController::class, 'index'])->name('threads.index');
-Route::delete('threads/{channel}/{thread}', [ThreadController::class, 'destroy'])->name('threads.delete');
+Route::controller(ThreadController::class)->group(function()
+{
+    Route::get('threads/create', 'create')->name('threads.create');
+    Route::get('threads/{thread}/edit', 'edit')->name('threads.edit');
+    Route::patch('threads/{thread}/update', 'update')->name('threads.update');
+    Route::get('threads/{channel}/{thread}', 'show')->name('threads.show');
+    Route::post('threads', 'store')->name('threads.store')->middleware(ConfirmedEmailCanCreate::class);
+    Route::get('threads/{channel?}', 'index')->name('threads.index');
+    Route::delete('threads/{channel}/{thread}', 'destroy')->name('threads.delete');
 
-Route::post('threads/{channel}/{thread}/lock', [ThreadController::class, 'lock'])->name('threads.lock')->middleware(Admin::class);
-Route::post('threads/{channel}/{thread}/unlock', [ThreadController::class, 'unlock'])->name('threads.unlock')->middleware(Admin::class);
+    Route::post('threads/{channel}/{thread}/lock', 'lock')->name('threads.lock')->middleware(Admin::class);
+    Route::post('threads/{channel}/{thread}/unlock', 'unlock')->name('threads.unlock')->middleware(Admin::class);
+});
+
+Route::controller(ReplyController::class)->group(function()
+{
+    Route::get('/threads/{channel}/{thread}/replies', 'index')->name('replies.index');
+    Route::post('/threads/{channel}/{thread}/replies', 'store')->name('threads-reply.store');
+    Route::patch('replies/{reply}', 'update')->name('replies.update');
+    Route::delete('replies/{reply}', 'destroy')->name('replies.delete');
+});
+Route::post('/replies/{reply}/mark-best',[BestReplyController::class, 'store'])->name('mark-best-reply.store');
+Route::post('/replies/{reply}/un-mark-best',[BestReplyController::class, 'destroy'])->name('un-mark-best-reply.destroy');
 
 Route::post('/replies/{reply}/favorites', [FavoritesController::class, 'store'])->name('favorites.store');
 Route::delete('/replies/{reply}/favorites', [FavoritesController::class, 'destroy'])->name('favorites.delete');
 
-Route::get('/threads/{channel}/{thread}/replies', [ReplyController::class, 'index'])->name('replies.index');
-Route::post('/threads/{channel}/{thread}/replies', [ReplyController::class, 'store'])->name('threads-reply.store');
-Route::patch('replies/{reply}', [ReplyController::class, 'update'])->name('replies.update');
-Route::delete('replies/{reply}', [ReplyController::class, 'destroy'])->name('replies.delete');
-
-Route::post('/replies/{reply}/mark-best',[BestReplyController::class, 'store'])->name('mark-best-reply.store');
-Route::post('/replies/{reply}/un-mark-best',[BestReplyController::class, 'destroy'])->name('un-mark-best-reply.destroy');
-
-Route::post('/threads/{channel}/{thread}/subscriptions', [ThreadSubscriptionController::class, 'store'])->name('threads-subsriptions.store')->middleware('auth');
-Route::delete('/threads/{channel}/{thread}/subscriptions', [ThreadSubscriptionController::class, 'destroy'])->name('threads-subsriptions.delete')->middleware('auth');
+Route::controller(ThreadSubscriptionController::class)->group(function()
+{
+    Route::post('/threads/{channel}/{thread}/subscriptions', 'store')->name('threads-subsriptions.store');
+    Route::delete('/threads/{channel}/{thread}/subscriptions', 'destroy')->name('threads-subsriptions.destroy');
+})->middleware('auth');
 
 
 Route::get('/dashboard', function () {
@@ -73,6 +81,7 @@ Route::middleware('auth')->group(function () {
 
     Route::post('/api/users/{user}/avatar', [UserAvatarController::class, 'store'])->name('users.avatar.store');
 });
+
 Route::get('clear_cache', function() {
     Redis::flushall();
 });
